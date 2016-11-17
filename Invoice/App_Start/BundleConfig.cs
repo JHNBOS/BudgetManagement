55301b3d3cbed8e99850e3d968d14d2367f8d8a4
Johan Bos <bosbosjohan@gmail.com>
2016-11-17 11:37:11 +0100

    Number Format correct and theming

--- Invoice/App_Start/BundleConfig.cs
+++ Invoice/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Invoice
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Cosmo.css",
                       "~/Content/site.css"));
         }
     }
--- Invoice/Models/producten.cs
+++ Invoice/Models/producten.cs
@@ -21,7 +21,7 @@ namespace Invoice
 
         public string Beschrijving { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
         public decimal? Prijs { get; set; }
 
         public int? BTW { get; set; }
